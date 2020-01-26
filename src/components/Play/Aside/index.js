117e96f673c912bb0fc8e1c2e7ad0c0f0287fc8f
Gerson <gersoncafilho@gmail.com>
2020-01-26 18:58:13 -0300

    Shadow aside more in aside left>> justify-content: space-between;

--- src/components/Play/Aside/index.js
+++ src/components/Play/Aside/index.js
@@ -2,9 +2,34 @@ import React from 'react';
 import './styles.css';
 
 export default function Aside({ children, bg }) {
+
+  function createImg() {
+    const vet = [1, 2, 3, 4, 5, 6];
+    return (
+      vet.map(index => {
+        return (
+          <header key={index} className="icon top">
+            <img src="https://s1.1zoom.me/big0/924/412182-blackangel.jpg"></img>
+
+          </header>
+        )
+      })
+    )
+  }
   return (
     <aside className={`aside ${bg}`}>
       {children}
+
+      <div className="container-top">
+        {createImg()}
+      </div>
+
+      <div className="container-button">
+        <header className="icon button">
+          <img src="https://s1.1zoom.me/big0/924/412182-blackangel.jpg"></img>
+        </header>
+      </div>
+
     </aside>
   );
 }
--- src/pages/Play/index.js
+++ src/pages/Play/index.js
@@ -6,26 +6,14 @@ import './styles.css';
 
 export default function Play() {
 
-  const classBG = ["green", "blue"];
+  const classBG = ["green", "grey"];
 
-  function createImg() {
-    const vet = [1, 2, 3, 4, 5, 6, 10];
-    return (
-      vet.map(index => {
-        return (
-          <header key={index} className="icon">
-            <img src="https://s1.1zoom.me/big0/924/412182-blackangel.jpg"></img>
-            <a>{index}</a>
-          </header>
-        )
-      })
-    )
-  }
+
   return (
     <div className="container">
       <Aside bg={classBG[0]}>
 
-        {createImg()}
+
 
       </Aside>
       <Center />
