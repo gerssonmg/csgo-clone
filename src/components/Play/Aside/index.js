dbd019acf833024e7b91d0c19ba69589f8ab4e32
Gerson <gersoncafilho@gmail.com>
2020-01-26 19:51:19 -0300

    logic to render from the aside right

--- src/components/Play/Aside/index.js
+++ src/components/Play/Aside/index.js
@@ -1,34 +1,57 @@
 import React from 'react';
 import './styles.css';
 
-export default function Aside({ children, bg }) {
+export default function Aside({ bg }) {
 
+  function lineRender(index) {
+
+    if (index === 1 || index === 3) {
+      return (
+        <div className="border-button">
+        </div>
+      )
+    }
+  }
   function createImg() {
     const vet = [1, 2, 3, 4, 5, 6];
     return (
       vet.map(index => {
         return (
-          <header key={index} className="icon top">
-            <img src="https://s1.1zoom.me/big0/924/412182-blackangel.jpg"></img>
-
-          </header>
+          <>
+            <header key={index} className="icon top">
+              <img alt="icone" src="https://s1.1zoom.me/big0/924/412182-blackangel.jpg"></img>
+            </header>
+            <>
+              {
+                lineRender(index)
+              }
+            </>
+          </>
         )
       })
     )
   }
+
+  function renderRoot() {
+    if (bg === 'grey-left') {
+      return (
+        <>
+          <div className="container-top">
+            {createImg()}
+          </div>
+          <div className="container-button">
+            <header className="icon button">
+              <img alt="icone" src="https://s1.1zoom.me/big0/924/412182-blackangel.jpg"></img>
+            </header>
+          </div>
+        </>
+      )
+    }
+  }
   return (
     <aside className={`aside ${bg}`}>
-      {children}
 
-      <div className="container-top">
-        {createImg()}
-      </div>
-
-      <div className="container-button">
-        <header className="icon button">
-          <img src="https://s1.1zoom.me/big0/924/412182-blackangel.jpg"></img>
-        </header>
-      </div>
+      {renderRoot()}
 
     </aside>
   );
--- src/pages/Play/index.js
+++ src/pages/Play/index.js
@@ -6,7 +6,7 @@ import './styles.css';
 
 export default function Play() {
 
-  const classBG = ["green", "grey"];
+  const classBG = ["grey-left", "grey-right"];
 
 
   return (
